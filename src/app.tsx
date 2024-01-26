1a9f5803a137bc00496cb8ec51ef9fb445126e44
analystdbrp <analyst.dbrp@gmail.com>
2024-01-26 16:23:03 +0530

    add svg file as inline loader

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,11 @@
 import './styles.css'
 import IMAGE from './1.jpg'
+import SVG from './samplesvg.svg'
 
 export const App = () => {
     return <>
     <h1>React Typescript Webpack Starter Template.</h1>
     <img src={IMAGE} alt="It is a BG Color" width="300" height="200"></img>
+    <img src={SVG} alt="It is a svg" width="300" height="200"></img>
     </>
 }
