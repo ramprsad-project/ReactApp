1dbad0e42fb8e7b04f65773000ff4bbcad84eeb1
analystdbrp <analyst.dbrp@gmail.com>
2024-02-10 09:02:48 +0530

    Successful Build

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,11 @@
-import './styles.css'
-import IMAGE from './1.jpg'
-import SVG from './logo.svg'
 
-const App = () => {
-    return <>
-    <h1>React Typescript Webpack Starter Template. - {process.env.NODE_ENV} {process.env.name}</h1>
-    <img src={IMAGE} alt="It is a BG Color" width="300" height="200"></img>
-    <img src={SVG} alt="It is a svg" width="300" height="200"></img>
+
+export default function App() {
+  return (
+    <>
+      <div className="clipping-container">
+    <h1>test</h1>
+      </div>
     </>
+  );
 }
-
-export default App;
