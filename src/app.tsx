4439b55acf5ff7e0055cfaaa89f2b0934adc44d6
analystdbrp <analyst.dbrp@gmail.com>
2024-02-11 16:09:13 +0530

    code cleanup

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,21 @@
-
+import { useState } from "react";
 
 export default function App() {
+
+  const [val,seteval] = useState(0);
+
+  const update = (value: number): void => {
+      seteval(value + val);
+      console.log(val);
+  }
+
   return (
     <>
       <div className="clipping-container">
-    <h1>test</h1>
+        <h1>test</h1>
+        <h2>{val}</h2>
       </div>
+      <button onClick={() => update(1)}>+</button>
     </>
   );
 }
